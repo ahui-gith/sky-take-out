e43df39814397aa74c18a455792a9e19cba3ef75
ahui <lijiahui2002@foxmail.com>
2025-09-29 16:44:06 +0800

    苍穹外卖后端-订单支付

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,4 +20,12 @@ public interface UserMapper {
      */
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
+
+    /**
+     * 根据userId查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
