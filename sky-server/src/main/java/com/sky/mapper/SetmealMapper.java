d75b96509df34293d71e9303ac8af00f5569b6ae
阿辉 <lijiahui2002@foxmail.com>
2025-10-05 09:39:15 +0800

    苍穹外卖后端-管理端-工作台

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -84,4 +85,12 @@ public interface SetmealMapper {
      */
     @Select("select * from setmeal where category_id = #{categoryId}")
     List<Setmeal> list(Integer categoryId);
+
+    /**
+     * 根据条件统计套餐数量
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
